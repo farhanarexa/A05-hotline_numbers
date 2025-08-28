892427e676fa70f6e92d9352a208fcaae062a9c2
farhanarexa <mailfarhana702@gmail.com>
2025-08-29 01:55:52 +0600

    implimented: dynamic call history clear

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let copyButtons = document.querySelectorAll(".copy-button"); // Get all copy buttons
 let callButtons = document.querySelectorAll(".call-button"); // Get all call buttons
+let clearButtons = document.getElementById("clearBtn"); // Get the clear button
 
 // heart count
 function heartCount() {
@@ -44,7 +45,7 @@ callButtons.forEach(function (button) {
         let coinCounter = document.getElementById("coinCounter").innerHTML;
         let coinCount = parseInt(coinCounter);
         if (coinCount < 20) {
-            alert("You don't have enough coins");
+            alert("You need at least 20 coins to make a call!");
             return;
         }
         coinCount -= 20;
@@ -56,7 +57,7 @@ callButtons.forEach(function (button) {
         let numberText = card.querySelector(".serviceNumber").innerText;
         alert("Calling " + serviceName + " - " + numberText);
 
-
+        // adding call history
         document.getElementById("callHistoryList").innerHTML += `
         <li class="bg-gray-50 rounded-lg p-3 border border-gray-100">
                     <div class="flex justify-between items-start">
@@ -67,10 +68,11 @@ callButtons.forEach(function (button) {
                         <span class="text-xs text-gray-500">${new Date().toLocaleTimeString()}</span>
                     </div>
                 </li>` 
-
-
-
-
-
     });
+});
+
+
+// clear call history
+clearButtons.addEventListener("click", function () {
+    document.getElementById("callHistoryList").innerHTML = "";
 });
