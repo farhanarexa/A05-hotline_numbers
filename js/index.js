211918d3434017c77281ca1fc12d396df653e131
farhanarexa <mailfarhana702@gmail.com>
2025-08-29 00:42:33 +0600

    implimented: functioning copy button

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+// heart count
 function heartCount() {
     let counterElement = document.getElementById("heartCounter");
     let currentCount = parseInt(counterElement.innerHTML);
     let newCount = currentCount + 1;
     counterElement.innerHTML = newCount;
-  }
+}
+
+
+// Get all copy buttons
+let copyButtons = document.querySelectorAll(".copy-button");
+copyButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        let card = button.closest(".bg-white");
+        let serviceName = card.querySelector("h3").innerText;
+        let numberText = card.querySelector(".serviceNumber").innerText;
+        navigator.clipboard.writeText(numberText).then(function () {
+            alert(serviceName + " - " + numberText + " has been copied!");
+        });
+    });
+});
+
